e0dc5d92c8851e8ecff1dc2acce11b7d924cd256
GitHub Action <action@github.com>
2022-08-05 14:02:19 +0000

    Template cleanup

--- src/main/kotlin/com/github/lilittlecat/intellijpangu/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/lilittlecat/intellijpangu/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.lilittlecat.intellijpangu.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.lilittlecat.intellijpangu.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/lilittlecat/intellijpangu/services/MyProjectService.kt
+++ src/main/kotlin/com/github/lilittlecat/intellijpangu/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.lilittlecat.intellijpangu.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.lilittlecat.intellijpangu.MyBundle
 
 class MyProjectService(project: Project) {
 
