eff6f8735e2621b6bef1fde77b7ba685c665eac0
LiLittleCat <luoyukongchan@outlook.com>
2022-08-06 20:18:53 +0800

    Optimize action

--- src/main/java/com/lilittlecat/plugin/action/PanguFormatAction.java
+++ src/main/java/com/lilittlecat/plugin/action/PanguFormatAction.java
@@ -1,14 +1,10 @@
 package com.lilittlecat.plugin.action;
 
-import com.intellij.lang.Language;
 import com.intellij.notification.Notification;
 import com.intellij.notification.NotificationDisplayType;
 import com.intellij.notification.NotificationGroup;
 import com.intellij.notification.Notifications;
 import com.intellij.openapi.actionSystem.*;
-import com.intellij.openapi.application.Application;
-import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.application.Result;
 import com.intellij.openapi.application.WriteAction;
 import com.intellij.openapi.command.CommandProcessor;
 import com.intellij.openapi.editor.Document;
@@ -16,28 +12,25 @@ import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.editor.SelectionModel;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.MessageType;
-import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
-import com.intellij.refactoring.rename.inplace.InplaceRefactoring;
-import org.jetbrains.annotations.NotNull;
 import ws.vinta.pangu.Pangu;
 
-import java.util.Objects;
+import java.text.MessageFormat;
 
 /**
  * @author LiLittleCat
  * @since 2022/8/6
  */
 public class PanguFormatAction extends AnAction {
+    private static final String GROUP_ID = "Pangu Format";
 
-    private static final NotificationGroup NOTIFICATION_GROUP = new NotificationGroup("Pangu Format", NotificationDisplayType.BALLOON, true);
+    private static final NotificationGroup NOTIFICATION_GROUP = new NotificationGroup(GROUP_ID, NotificationDisplayType.BALLOON, true);
 
     private static final Pangu PANGU = new Pangu();
 
     @Override
     public void actionPerformed(AnActionEvent e) {
         DataContext dataContext = e.getDataContext();
-        Language[] languages = LangDataKeys.CONTEXT_LANGUAGES.getData(dataContext);
         Editor editor = CommonDataKeys.EDITOR.getData(dataContext);
         PsiFile file = CommonDataKeys.PSI_FILE.getData(dataContext);
         if (editor == null || file == null) {
@@ -51,7 +44,11 @@ public class PanguFormatAction extends AnAction {
                 // no selection, pangu all content of current file
                 String text = document.getText();
                 String formattedText = PANGU.spacingText(text);
-                document.setText(formattedText);
+                CommandProcessor.getInstance().executeCommand(file.getProject(), () -> {
+                    WriteAction.run(() -> {
+                        document.setText(formattedText);
+                    });
+                }, GROUP_ID, null);
             } else if (!selectedText.isBlank()) {
                 // selection, pangu selected text
                 int selectionStart = selectionModel.getSelectionStart();
@@ -61,18 +58,28 @@ public class PanguFormatAction extends AnAction {
                     WriteAction.run(() -> {
                         document.replaceString(selectionStart, selectionEnd, formattedText);
                     });
-                }, "Pangu Format", null);
+                }, GROUP_ID, null);
             }
-            Notification notification = NOTIFICATION_GROUP.createNotification("Pangu Format:  '" + file.getName() + "' success.", MessageType.INFO);
-            Notifications.Bus.notify(notification, e.getProject());
+            notification(MessageFormat.format("{0}: '{1}' success.", GROUP_ID, file.getName()), MessageType.INFO, e.getProject());
         } else {
             // notification: cannot edit current file
-            Notification notification = NOTIFICATION_GROUP.createNotification("Pangu Format fail: '" + file.getName() + "' is not writable.", MessageType.WARNING);
-            Notifications.Bus.notify(notification, e.getProject());
+            notification(MessageFormat.format("{0} fail: '{1}' is not writable.", GROUP_ID, file.getName()), MessageType.WARNING, e.getProject());
         }
 
 //        Editor editor = e.getRequiredData(CommonDataKeys.EDITOR);
 //        Project project = e.getRequiredData(CommonDataKeys.PROJECT);
 //        String name = e.getRequiredData(CommonDataKeys.PSI_FILE).getViewProvider().getVirtualFile().getName();
+    }
+
+    /**
+     * notification
+     *
+     * @param message     message to show
+     * @param messageType message type
+     * @param project     current project
+     */
+    private static void notification(String message, MessageType messageType, Project project) {
+        Notification notification = NOTIFICATION_GROUP.createNotification(message, messageType);
+        Notifications.Bus.notify(notification, project);
     }
 }
